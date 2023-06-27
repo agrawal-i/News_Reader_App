be973cdbda843c6b42723ccde18026e2d43ed0bc
agrawal-i <agraw.ishika@gmail.com>
2023-06-27 21:06:34 +0530

    News API linked

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web'
 
- const App = () => {
+import NewsCards from './components/NewsCards/NewsCards';
 
+const alanKey = '7408951129dc3c5e5b579565073c5c292e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+ const App = () => {
+    const [newsArticles , setNewsArticles]= useState([]);
     useEffect(() => {
-        alanBtn()
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles }) => {
+                if(command === 'newHeadlines'){
+                    setNewsArticles(articles);
+                }
+            }
+        })
     },[])
 return (
     <div> 
         <h1> Alan AI NEWS Application</h1>
+        <NewsCards articles= {newsArticles}/>
     </div>
 );
  
