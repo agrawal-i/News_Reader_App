71c6b233b874372ec8480465bb76ebe79034ffd3
agrawal-i <agraw.ishika@gmail.com>
2023-06-29 20:53:06 +0530

    made all cards functional

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React,{useState,useEffect,createRef} from 'react';
 import useStyles from './styles.js'
+import classNames from 'classnames';
+
 import { Card, CardActions , CardActionArea , CardContent,CardMedia, Button , Typography} from '@material-ui/core'; 
-const NewsCard = ({ article: { description , publishedAt, source,title,url,urlToImage },i }) => {
+
+const NewsCard = ({ article: { description , publishedAt, source,title,url,urlToImage },i ,activeArticle}) => {
     const classes=useStyles();
+    const [elRefs,setElRefs]=useState([]);
+    const scrollToRef =(ref) => window.scroll(0,ref.current.offsetTop-50);
+
+    useEffect(()=>{
+        setElRefs((refs)=>Array(20).fill().map((_,j) => refs[j] || createRef()));
+    },[]);
+    useEffect(() => {
+        if(i === activeArticle && elRefs[activeArticle]){
+             scrollToRef(elRefs[activeArticle]);
+        }
+    },[i,activeArticle,elRefs])
     return (
-        <Card className={classes.card}>
+        <Card ref={elRefs[i]} className={ classNames(classes.card, activeArticle===i ?classes.activeCard :null)}>
             <CardActionArea href={url} target="_blank">
                 <CardMedia className={classes.media} image={urlToImage || 'https://st3.depositphotos.com/29384342/33494/i/1600/depositphotos_334948458-stock-photo-stylish-template-modern-font.jpg'}/>
                 <div className={classes.details}>
